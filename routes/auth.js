af856793756ad97ec643aa3f3e2223d31bdc06ef
Albert Ulloa Mina <142063470+Ul10a@users.noreply.github.com>
2025-05-16 11:25:12 -0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,8 @@ const authController = require('../controllers/authcontroller');
 const enviarCorreo = require('../utils/mailer'); // ✅ Importa la función para enviar correos
 
 // Mostrar formularios de registro e inicio de sesión
-router.get('/register', authController.showRegister);
-router.post('/register', authController.register);
+router.get('/Register', authController.showRegister);
+router.post('/Register', authController.register);
 router.get('/login', authController.showLogin);
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
@@ -18,6 +18,6 @@ router.get('/forgot-password', authController.getForgotPassword);
 router.post('/forgot-password', authController.postForgotPassword);
 
 // Ruta protegida para el dashboard
-router.get('/dashboard', authController.dashboard);
+router.get('/Dashboard', authController.dashboard);
 
 module.exports = router;
