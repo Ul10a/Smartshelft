10b572141a06a30f2017eba2df4a57bd7becfb53
Albert Ulloa Mina <142063470+Ul10a@users.noreply.github.com>
2025-05-16 16:56:35 -0500

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo'); 
 const session = require('express-session');
 const dotenv = require('dotenv');
 const path = require('path');
